a5d3fa61f499d44b3001a9d83d7ed24bf45ba922
ABBOSXAN <absalomov.xan@gmail.com>
2023-10-03 10:43:04 +0500

    News

---
+++ news/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import News
+
+
+# Create your views here.
+
+class HomePageView(ListView):
+    model = News
+    template_name = 'home.html'
